302c65667181093a60330f00414e97e39e6d9a8f
exloncus <86781298+exloncus@users.noreply.github.com>
2021-11-14 20:40:15 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# saskepom
+# CODEMAREFİ
+
+CodeMareFi - Coded By $adista
+
+    Ayrıca dostlarım ready.js yok, onu bot.js'ye
+    attım bakarsınız oradan. Hadi herkese iyi kullanımlar :)
---
+++ komutlar/CodeMareFi.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js')
+
+    exports.run = async(client, message, args) => {
+        message.channel.send('https://images-ext-2.discordapp.net/external/POjRfMzRP1GnCNRpXE2ASM-9N6V5NMe1dA5pN0YATEs/%3Fwidth%3D960%26height%3D540/https/media.discordapp.net/attachments/785313661811687465/806182089413623808/CMF2.png?width=400&height=225')
+    }
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: ['CodeMareFi'],
+    permLevel: 0
+}
+
+exports.help = {
+    name: 'codemarefi'
+}
